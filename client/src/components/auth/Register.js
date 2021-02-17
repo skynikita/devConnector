21c9d56f8ded1c969211d24907e4ef86d00ba834
skynikita <yangluo1105@gmail.com>
2021-02-17 15:55:03 +1100

    update Login UI

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,4 @@
 import React, {Fragment, useState} from 'react';
-import axios from 'axios';
 import {Link } from 'react-router-dom';
 
 const Register = () => {
@@ -9,9 +8,7 @@ const Register = () => {
         password:'',
         password2:''
     });
-
     const { name, email, password, password2 } = formData;
-
     const onChange = e => setFormData({
         ...formData, [e.target.name]: e.target.value
     });
@@ -21,24 +18,7 @@ const Register = () => {
         if(password !== password2){
             console.log('Passwords do not match')
         }else{
-            const newUser ={
-                name,
-                email,
-                password
-            };
-
-        try {
-            const config ={
-                headers:{
-                    'Content-Type': 'application/json'
-                }
-            }
-            const body = JSON.stringify(newUser);
-            const res = await axios.post('/api/users',body,config);
-            console.log(res.data);
-        }catch(err) {
-            console.error(err.response.data);
-        }
+           console.log('SUCCESS')
         }
     };
 
