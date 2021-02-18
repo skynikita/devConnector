da3c560e2405d23edc492a5a7c67c5a6cc6cbe64
skynikita <yangluo1105@gmail.com>
2021-02-18 18:47:53 +1100

    Register and login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,10 @@
 import React, {Fragment, useState} from 'react';
 import {Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import {login} from '../../actions/auth'
 
-const Login = () => {
+const Login = ({ login }) => {
     const [formData, setFormData] = useState({
         email:'',
         password:'',
@@ -15,7 +18,7 @@ const Login = () => {
 
     const onSubmit = async e => {
         e.preventDefault();
-        console.log('SUCCESS')
+        login(email, password);
     };
 
 
@@ -47,4 +50,8 @@ const Login = () => {
     )
 }
 
-export default Login;
+Login.propTypes = {
+    login: PropTypes.func.isRequired,
+}
+
+export default connect(null,{ login })(Login);
