2a8d026e8e0ef42e014c03418ed54d55c54330fd
skynikita <yangluo1105@gmail.com>
2021-03-04 17:55:57 +1100

    update Dashboard setup Profile

---
+++ client/src/components/profile/CreateProfile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import propTypes from 'prop-types'
+import { connect } from 'react-redux';
+
+const CreateProfile = props => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+CreateProfile.prototype ={
+
+}
+
+export default CreateProfile
