28e55c7221f14b36ebdf4322a1524a3ebce4c364
skynikita <yangluo1105@gmail.com>
2021-05-13 16:59:59 +1000

    Set profile theme

--- client/src/components/profileDetails/Profile.js
+++ client/src/components/profileDetails/Profile.js
@@ -1,17 +1,29 @@
 import React, { Fragment, useEffect} from 'react';
+import ProfileTop from "./ProfileTop"
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import Spinner from "../layout/Spinner";
 import { getProfileById } from "../../actions/profile";
+import {Link} from "react-router-dom";
 
 const Profile = ({ match, getProfileById, profile: {profile, loading}, auth }) => {
     useEffect(()=>{
         getProfileById(match.params.id)
-    },[getProfileById])
+    },[getProfileById, match.params.id])
     return(
         <Fragment>
-            profile
-
+            {profile === null || loading ?( <Spinner /> ):
+                ( <Fragment>
+                <Link to="/profiles" className="btn btn-light">
+                    Back To Profiles
+                </Link>
+                {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id
+                && (<Link to='/edit-profile' className="btn badge-dark">Edit Profile</Link>)}
+            </Fragment>
+                )}
+            <div className="profile-grid my-1">
+                <ProfileTop profile={profile}/>
+            </div>
         </Fragment>
     )
 }
