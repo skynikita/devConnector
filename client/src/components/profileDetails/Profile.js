ca9fcc16e37aa0a35bcaae678bff6a22ca571c21
skynikita <yangluo1105@gmail.com>
2021-05-14 23:33:08 +1000

    ProfileTop and ProfileAbout Components

--- client/src/components/profileDetails/Profile.js
+++ client/src/components/profileDetails/Profile.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useEffect} from 'react';
 import ProfileTop from "./ProfileTop"
+import ProfileAbout from "./ProfileAbout"
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import Spinner from "../layout/Spinner";
@@ -12,18 +13,20 @@ const Profile = ({ match, getProfileById, profile: {profile, loading}, auth }) =
     },[getProfileById, match.params.id])
     return(
         <Fragment>
-            {profile === null || loading ?( <Spinner /> ):
+            {profile === null || loading ? ( <Spinner /> ):
                 ( <Fragment>
                 <Link to="/profiles" className="btn btn-light">
                     Back To Profiles
                 </Link>
                 {auth.isAuthenticated && auth.loading === false && auth.user._id === profile.user._id
-                && (<Link to='/edit-profile' className="btn badge-dark">Edit Profile</Link>)}
+                && (<Link to='/edit-profile' className="btn badge-dark">Edit Profile</Link>
+                )}
+                        <div className="profile-grid my-1">
+                            <ProfileTop profile={profile}/>
+                            <ProfileAbout profile={profile} />
+                        </div>
             </Fragment>
                 )}
-            <div className="profile-grid my-1">
-                <ProfileTop profile={profile}/>
-            </div>
         </Fragment>
     )
 }
