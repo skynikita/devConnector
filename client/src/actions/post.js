6903699563224b45522c70a5943d56d138047a09
skynikita <yangluo1105@gmail.com>
2021-05-18 12:24:51 +1000

    Single Post Display

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -5,7 +5,8 @@ import {
     GET_POSTS,
     POST_ERROR,
     UPDATE_LIKES,
-    ADD_POST
+    ADD_POST,
+    GET_POST
 } from "./types";
 
 // GET posts
@@ -91,6 +92,23 @@ export const addPost = formData => async dispatch => {
             payload: res.data
         })
         dispatch(setAlert('Post Created','success'))
+    }catch(err){
+        dispatch({
+            type: POST_ERROR,
+            payload: {msg: err.response.statusText, status: err.response.status}
+        })
+
+    }
+}
+
+// GET post
+export const getPost = id => async dispatch => {
+    try{
+        const res = await axios.get(`/api/posts/${id}`);
+        dispatch({
+            type: GET_POST,
+            payload: res.data
+        })
     }catch(err){
         dispatch({
             type: POST_ERROR,
--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -6,7 +6,14 @@ import { connect } from 'react-redux'
 import {addLike,removeLike,deletePost} from "../../actions/post";
 
 
-const PostItem = ({auth, post: {_id, text, name, avatar, user, likes, comments, date },addLike, removeLike ,deletePost}) => {
+const PostItem = ({
+                      auth,
+                      post: {_id, text, name, avatar, user, likes, comments, date },
+                      addLike,
+                      removeLike,
+                      deletePost,
+                      showActions
+}) => {
     return (
         <div className="post bg-white p-1 my-1">
             <div>
@@ -26,35 +33,42 @@ const PostItem = ({auth, post: {_id, text, name, avatar, user, likes, comments, 
                 <p className="post-date">
                     Posted on {moment(date).format('DD/MM/YYYY')}
                 </p>
-                <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
-                    <i className="fas fa-thumbs-up"/>{' '}
-                    <span>{
-                        likes.length > 0 && (
-                            <span>{likes.length}</span>
-                        )
-                    }</span>
-                </button>
-                <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
-                    <i className="fas fa-thumbs-down"/>
-                </button>
-                <Link to={`/post/${_id}`} className="btn btn-primary">
-                    Discussion {
-                    comments.length > 0 && (
-                        <span className='comment-count'>{comments.length}</span>
-                    )
-                }
-                </Link>
-                {!auth.loading && user === auth.user._id && (
-                    <button
-                        onClick={e => deletePost(_id)}
-                        type="button"
-                        className="btn btn-danger">
-                        <i className="fas fa-times"/>
+                {showActions && <Fragment>
+                    <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
+                        <i className="fas fa-thumbs-up"/>{' '}
+                        <span>{
+                            likes.length > 0 && (
+                                <span>{likes.length}</span>
+                            )
+                        }</span>
                     </button>
-                )}
+                    <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
+                        <i className="fas fa-thumbs-down"/>
+                    </button>
+                    <Link to={`/posts/${_id}`} className="btn btn-primary">
+                        Discussion {' '}{
+                        comments.length > 0 && (
+                            <span className='comment-count'>{comments.length}</span>
+                        )
+                    }
+                    </Link>
+                    {!auth.loading && user === auth.user._id && (
+                        <button
+                            onClick={e => deletePost(_id)}
+                            type="button"
+                            className="btn btn-danger">
+                            <i className="fas fa-times"/>
+                        </button>
+                    )}
+                </Fragment>
+                }
             </div>
         </div>
     )
+}
+
+PostItem.defaultProps = {
+    showActions: true
 }
 
 PostItem.prototype = {
