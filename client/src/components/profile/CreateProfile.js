8fdae96ef40928458fd003978b411353c672b993
skynikita <yangluo1105@gmail.com>
2021-05-18 17:58:31 +1000

    bug fix

--- client/src/components/profile/CreateProfile.js
+++ client/src/components/profile/CreateProfile.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useState} from 'react'
-import{Link,withRouter,useHistory} from 'react-router-dom'
+import{Link,withRouter} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
 import {createProfile} from "../../actions/profile";
